cd0b78bcf0a7ad3a39748af0ef5bb525f82fb217
unknown <02230261.cst@rub.edu.bt>
2024-03-21 13:55:04 +0600

    Student_grade.py

--- Test.py
+++ Test.py
@@ -1,7 +1,8 @@
-print(str(input("Enter your name:")))
-
-x = 
-print(int(input("Enter number 1:")))
-
+sum =0
+i = 0
+while i<10:
+    sum += i 
+    print(sum)
+    i += 1 
 
 
--- force.py
+++ force.py
@@ -8,24 +8,25 @@ print("2. Acceleration(a)")
 print("3. Force(F)")
 missing_value_choice = input("Enter the option number for the missing valuse: ")
 
+while True:
 # Prompt the user to enter the other two values
-if missing_value_choice == "1":
-    a = float(input("Enter acceleration (a): "))
-    F = float(input("Enter force (F): "))
-    m = F / a
-    print(f"Mass (m) = {m}")
+    if missing_value_choice == "1":
+        a = float(input("Enter acceleration (a): "))
+        F = float(input("Enter force (F): "))
+        m = F / a
+        print(f"Mass (m) = {m}")
 
-elif missing_value_choice == "2":
-    m = float(input("Enter mass (m): "))
-    F = float(input("Enter force (F): "))
-    a = F / m 
-    print(f"Acceleration (a) = {a}")
+    elif missing_value_choice == "2":
+        m = float(input("Enter mass (m): "))
+        F = float(input("Enter force (F): "))
+        a = F / m 
+        print(f"Acceleration (a) = {a}")
 
-elif missing_value_choice == "3":
-    m = float(input("Enter mass (m): "))
-    a = float(input("Enter acceleration (a): "))
-    F = m * a
-    print(f"Force (F) = {F}")  
+    elif missing_value_choice == "3":
+        m = float(input("Enter mass (m): "))
+        a = float(input("Enter acceleration (a): "))
+        F = m * a
+        print(f"Force (F) = {F}")  
 
-else:
-    print("Invalid option selected for the missing value.") 
+    else:
+        print("Invalid option selected for the missing value.") 
